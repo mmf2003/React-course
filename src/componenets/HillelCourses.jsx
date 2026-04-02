d0401b550e69b0938fd2ba2cb16e67c5700c48b9
mmf2003 <mmf2003@ukr.net>
2026-04-02 16:18:55 +0300

    Created new folder - Componenets and new file - HillelCourses

---
+++ src/componenets/HillelCourses.jsx
@@ -0,0 +1,15 @@
+
+const HillelCourses = () => {
+    return (
+        <aside>
+            <h2>Hillel Courses</h2>
+            <ul>
+                <li>JS Basic</li>
+                <li>JS Pro</li>
+                <li>React JS</li>
+            </ul>
+        </aside>
+    )       
+}
+
+export default HillelCourses;
